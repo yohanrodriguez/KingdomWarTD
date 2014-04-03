05bb6d062044708e435c67901d6d593e27666c7e
yohanrodriguez <yohanrodriguez@github.com>
2014-04-03 11:25:56 +0200

    Adding Android Support

--- Android/src/fr/nwg/kingdomwar/android/KingdomWarActivity.java
+++ Android/src/fr/nwg/kingdomwar/android/KingdomWarActivity.java
@@ -1,15 +1,18 @@
 package fr.nwg.kingdomwar.android;
 
-import android.app.Activity;
 import android.os.Bundle;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import fr.nwg.kingdomwar.KingdomWarGame;
 
-public class KingdomWarActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+public class KingdomWarActivity extends AndroidApplication {
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.useGL20 = false;
+
+        initialize(new KingdomWarGame(), cfg);
     }
 }
